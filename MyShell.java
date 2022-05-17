64101adb28c99953d7272bf80cfa05e02d49f8d2
Matthias <12-build@web.de>
2022-05-17 12:41:06 +0200

    unused code/imports

--- MyShell.java
+++ MyShell.java
@@ -1,12 +1,9 @@
 import java.io.File;
-import java.io.FileNotFoundException;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 import java.util.Scanner;
 
-import static cTools.KernelWrapper.STDIN_FILENO;
-import static cTools.KernelWrapper.STDOUT_FILENO;
 import static cTools.KernelWrapper.*;
 
 public class MyShell {
@@ -267,14 +264,14 @@ class AndSplit {
 
 class ShellLogger {
     private static final String ANSI_RESET = "\u001B[0m";
-    private static final String ANSI_BLACK = "\u001B[30m";
+    //private static final String ANSI_BLACK = "\u001B[30m";
     private static final String ANSI_RED = "\u001B[31m";
     private static final String ANSI_GREEN = "\u001B[32m";
-    private static final String ANSI_YELLOW = "\u001B[33m";
+    //private static final String ANSI_YELLOW = "\u001B[33m";
     private static final String ANSI_BLUE = "\u001B[34m";
-    private static final String ANSI_PURPLE = "\u001B[35m";
-    private static final String ANSI_CYAN = "\u001B[36m";
-    private static final String ANSI_WHITE = "\u001B[37m";
+    //private static final String ANSI_PURPLE = "\u001B[35m";
+    //private static final String ANSI_CYAN = "\u001B[36m";
+    //private static final String ANSI_WHITE = "\u001B[37m";
 
     private ShellLogger() {
         //Singleton pattern
